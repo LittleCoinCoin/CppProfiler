90cb03efdbb80becb19fa9011ee1159a8497c089
Jacopin Eliott <eliott.jacopin@laposte.net>
2024-03-18 12:44:50 +0900

    [Add] Test functions
    Major:
    - Testing the bandwidth measure
    - Testting adding to a second track

--- src/Tests/main.cpp
+++ src/Tests/main.cpp
@@ -10,18 +10,44 @@ void TestFunction(Profile::u64 _arr[], Profile::u64 _count)
 	}
 }
 
+void TestFunction_Bandwidth(Profile::u64 _arr[], Profile::u64 _count)
+{
+	PROFILE_FUNCTION_TIME_BANDWIDTH(0, sizeof(Profile::u64) * _count);
+
+	for (Profile::u64 i = 0; i < _count; ++i)
+	{
+		_arr[i] = i;
+	}
+}
+
+void TestFunction_Track2(Profile::u64 _arr[], Profile::u64 _count)
+{
+	PROFILE_FUNCTION_TIME(1);
+
+	for (Profile::u64 i = 0; i < _count; ++i)
+	{
+		_arr[i] = i;
+	}
+}
+
 int main()
 {
 	Profile::Profiler profiler("Tests");
 	Profile::SetProfiler(&profiler);
 	profiler.AddTrack("main");
 	profiler.Initialize();
-	profiler.tracks[0].Initialize();
-
 	Profile::u64 arr[8192];
-	TestFunction(arr, 8192);
 
+	profiler.tracks[0].Initialize();
+	TestFunction(arr, 8192);
+	TestFunction_Bandwidth(arr, 8192);
 	profiler.tracks[0].End();
+
+	profiler.AddTrack("subtrack");
+	profiler.tracks[1].Initialize();
+	TestFunction_Track2(arr, 8192);
+	profiler.tracks[1].End();
+
 	profiler.End();
 	profiler.Report();
 
