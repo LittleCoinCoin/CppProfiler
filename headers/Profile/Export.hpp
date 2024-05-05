9e0b2acec4bef9a35d1583549db4fd4b097569c5
Jacopin Eliott <eliott.jacopin@laposte.net>
2024-05-06 00:59:03 +0200

    [Update] Dll Export preprocessor macros definition
    Major:
    - GCC and CLANG on Ubuntu 22.04 of the runners of workflows raised an error with badly defined PROFILE_API. The macro was indeed not defined in some cases.
    - Going back to this file made me realize that it wasn't doing what I wanted even for windows. Because the `__dllimport` was never used.
    - Also, I want to keep the possibility to use the dlls in an application as well as direct building within a project.
    - So, I need an evac case where PROFILE_API expands to an empty string when the user decides to include the headers without using the dll (hence the headers in the app cannot be marked `__dllimport` since no `__dllexport` was processed)
    - This is the purpose of USE_PROFILE_LIB

--- headers/Profile/Export.hpp
+++ headers/Profile/Export.hpp
@@ -1,13 +1,19 @@
 #pragma once
 
-#if BUILD_PROFILER_LIB
-	#if HAVE_VISIBILITY
-		#define PROFILE_API __attribute__((__visibility__("default")))
-	#elif (defined _WIN32 && !defined __CYGWIN__) && BUILD_PROFILER_LIB
-		#define PROFILE_API __declspec(dllexport)
-	#elif (defined _WIN32 && !defined __CYGWIN__)
-		#define PROFILE_API __declspec(dllimport)
+#ifndef PROFILE_API
+	#ifdef _WIN32
+		#ifdef BUILD_PROFILER_LIB
+			#define PROFILE_API __declspec(dllexport)
+		#elif USE_PROFILER_LIB
+			#define PROFILE_API __declspec(dllimport)
+		#else
+			#define PROFILE_API
+		#endif
+	#else
+		#ifdef BUILD_PROFILER_LIB || USE_PROFILER_LIB
+			#define PROFILE_API __attribute__((__visibility__("default")))
+		#else
+			#define PROFILE_API
+		#endif
 	#endif
-#else
-	#define PROFILE_API
 #endif
