362071fded4cc09b6641ac560e5e9626c3723d04
LittleCoinCoin <eliott.jacopin@laposte.net>
2024-11-15 23:12:53 +0900

    [Add] Main Method of Profiler
    Major:
    - The methods which will be likely useful to the users in python.
    - For now I am not including the methods which I know are only supposed to be used internally.

--- src/Bindings/Python.cpp
+++ src/Bindings/Python.cpp
@@ -1,6 +1,7 @@
 #include "pybind11/pybind11.h"
 #include "Profile/Profiler.hpp"
 
+using namespace pybind11::literals;
 
 PYBIND11_MODULE(PyProfile, m)
 {
@@ -12,8 +13,21 @@ PYBIND11_MODULE(PyProfile, m)
 
     //Add the bindings to the Profile::Profiler class
 	pybind11::class_<Profile::Profiler>(m, "Profiler")
+
+		//Add the fields and properties to the class
 		.def_property("name",
 			[](Profile::Profiler& _self) { return _self.name; },
 			&Profile::Profiler::SetProfilerName)
-		.def(pybind11::init<>());
+
+		.def(pybind11::init<>())
+		.def("SetTrackName", &Profile::Profiler::SetTrackName, "Sets the name of a track.", "_trackIdx"_a, "_name"_a)
+		.def("Clear", &Profile::Profiler::Clear, "Clears the profiler's values as well as all its initialized tracks.")
+		.def("ClearTracks", &Profile::Profiler::ClearTracks, "Clears all used blocks of all used tracks in the profiler.")
+		.def("End", &Profile::Profiler::End, "Ends the profiler.")
+		.def("ExportToCSV", &Profile::Profiler::ExportToCSV, "Exports the profiling statistics of the profiler to a CSV file.", "_path"_a)
+		.def("Initialize", &Profile::Profiler::Initialize, "Starts the profiler.")
+		.def("Report", &Profile::Profiler::Report, "Outputs the profiling statistics of all tracks in the profiler.")
+		.def("Reset", &Profile::Profiler::Reset, "Resets the profiler's values as well as all its initialized tracks.")
+		.def("ResetTracks", &Profile::Profiler::ResetTracks, "Resets all used blocks of all used tracks in the profiler.");
+		
 }
